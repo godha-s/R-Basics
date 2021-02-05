a34ef0ce08623f17a19c264a49703e57361b160d
shanmuka <shanmuka.chowdary@gmail.com>
2021-02-05 17:20:44 -0500

    added vector basics into vector.R

---
+++ vectors.R
@@ -0,0 +1,11 @@
+# creating a vector using : operator
+x <- 1:7; x
+
+# create a vector with a range
+y <- 2:-2; y
+
+# creating the vector with the seq() function
+seq(1, 3, by=0.2)
+
+# specify the length of the vector
+seq(1, 5, length.out=4)
