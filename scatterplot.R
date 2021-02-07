0d7aab757c00471cc9b0de74896b21b1e79e2a8d
shanmuka <shanmuka.chowdary@gmail.com>
2021-02-07 12:41:12 -0500

    Added scatterplot.R with sales-temp example

---
+++ scatterplot.R
@@ -0,0 +1,21 @@
+# install the vcd package
+install.packages("vcd")
+
+# import the vcd library
+library(vcd)
+
+# initializing the sales vector data as per the instruction document
+sales <- c(10, 10, 12, 15, 20, 15, 25, 9, 8, 26)
+
+# initializing the temperature vector data as per the instruction document
+temperature <- c(69, 81, 78, 88, 81, 76, 83, 72, 60, 95)
+
+# plot the sales ~ temperature data
+plot(
+  temperature,
+  sales,
+  main = "sales ~ temperature scatter plot",
+  ylab = "Sales",
+  xlab = "Temperature" ,
+  col = "blue"
+)
