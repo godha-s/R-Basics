3dcb2e59e56a5c0cbf2ef8f60a4cee6ac4531eda
shanmuka <shanmuka.chowdary@gmail.com>
2021-02-06 21:58:37 -0500

    Added example to access the elements in vectors in vectors.R

--- vectors.R
+++ vectors.R
@@ -5,7 +5,16 @@ x <- 1:7; x
 y <- 2:-2; y
 
 # creating the vector with the seq() function
-seq(1, 3, by=0.2)
+seq(1, 3, by=0.3)
 
 # specify the length of the vector
 seq(1, 5, length.out=4)
+
+# access the 2nd element in x--> pass the element number
+x[2]
+
+# access two element in x vector
+x[c(2, 5)]
+
+# get all elements except 2nd element
+x[-2]
