bab8741735278c520f3620491d95409673e1338d
shanmuka <shanmuka.chowdary@gmail.com>
2021-02-08 23:28:11 -0500

    Added example for elements arrenged in sequentially by row and coloumn in matrix.R

--- matrix.R
+++ matrix.R
@@ -1,3 +1,11 @@
 # A 5 row and 2 column matrix of 10 integers
 yMatrix <- matrix(1:10, nrow = 5, ncol = 2)
 yMatrix
+
+# Elements are arranged sequentially by row.
+M <- matrix(c(3:14), nrow = 4, byrow = TRUE)
+print(M)
+
+# Elements are arranged sequentially by column.
+N <- matrix(c(3:14), nrow = 4, byrow = FALSE)
+print(N)
